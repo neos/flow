neos/flow-development-collection

[!!!][TASK] Minor typo corrections in code
This trivially corrects some typos in code.

As a consequence, the DataNotSerializeableException was renamed
to DataNotSerializableException.

Change-Id: I1693fa3a8383869f4b70d56b922439ae448b5f00
Releases: 1.1, 1.2
Original-Commit-Hash: fec3f046d336725bcbdbe39212ef313ec90d4552

TYPO3.Flow/Migrations/Code/Version201206271128.php
@@ -1,22 +1,28 @@
 <?php
-namespace TYPO3\FLOW3\Session\Exception;
+namespace TYPO3\FLOW3\Core\Migrations;
 
 /*                                                                        *
- * This script belongs to the FLOW3 framework.                            *
+ * This script belongs to the FLOW3 package "FLOW3".                      *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU Lesser General Public License, either version 3   *
- * of the License, or (at your option) any later version.                 *
+ *  of the License, or (at your option) any later version.                *
  *                                                                        *
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
 /**
- * A "Data not serializeable" Exception
- *
- * @api
+ * Replace DataNotSerializeableException with DataNotSerializableException.
  */
-class DataNotSerializeableException extends \TYPO3\FLOW3\Session\Exception {
+class Version201205292145 extends AbstractMigration {
+
+	/**
+	 * @return void
+	 */
+	public function up() {
+		$this->searchAndReplace('Session\Exception\DataNotSerializeableException', 'Session\Exception\DataNotSerializableException');
+	}
+
 }
 
 ?>

neos/flow

Original-Commit-Hash: c86356cca0346dcfa1327f47a0c24f5ea01fb7e8

Migrations/Code/Version201206271128.php
@@ -1,22 +1,28 @@
 <?php
-namespace TYPO3\FLOW3\Session\Exception;
+namespace TYPO3\FLOW3\Core\Migrations;
 
 /*                                                                        *
- * This script belongs to the FLOW3 framework.                            *
+ * This script belongs to the FLOW3 package "FLOW3".                      *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU Lesser General Public License, either version 3   *
- * of the License, or (at your option) any later version.                 *
+ *  of the License, or (at your option) any later version.                *
  *                                                                        *
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
 /**
- * A "Data not serializeable" Exception
- *
- * @api
+ * Replace DataNotSerializeableException with DataNotSerializableException.
  */
-class DataNotSerializeableException extends \TYPO3\FLOW3\Session\Exception {
+class Version201206271128 extends AbstractMigration {
+
+	/**
+	 * @return void
+	 */
+	public function up() {
+		$this->searchAndReplace('Session\Exception\DataNotSerializeableException', 'Session\Exception\DataNotSerializableException');
+	}
+
 }
 
 ?>