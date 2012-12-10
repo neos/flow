neos/flow-development-collection

[!!!][BUGFIX] Http\Uri constructor throws exception on invalid Uri
The constructor of the Uri object now checks the return value
of the parse_url method and throws an exception if this returns
anything else than a valuable array, because in this case the
given Uri is considered seriously malformed.

This is considered breaking because prior to this change,
the Uri just silently was created with all empty values.

Change-Id: I54c3c82e60c53ebad7dcf43ecd7e7f7044831668
Fixes: #42511
Releases: 1.1, 1.2
Original-Commit-Hash: 1b03b89c9a61c5a8066544929a9be590eb3521b3

TYPO3.Flow/Classes/TYPO3/Flow/Http/Uri.php
@@ -93,7 +93,12 @@ class Uri {
 	public function __construct($uriString) {
 		if (!is_string($uriString)) throw new \InvalidArgumentException('The URI must be a valid string.', 1176550571);
 
-		$uriParts = parse_url($uriString);
+		$parseUrlException = NULL;
+		try {
+			$uriParts = parse_url($uriString);
+		} catch(\TYPO3\Flow\Error\Exception $exception) {
+			$parseUrlException = $exception;
+		}
 		if (is_array($uriParts)) {
 			$this->scheme = isset($uriParts['scheme']) ? $uriParts['scheme'] : NULL;
 			$this->username = isset($uriParts['user']) ? $uriParts['user'] : NULL;
@@ -105,6 +110,8 @@ class Uri {
 				$this->setQuery ($uriParts['query']);
 			}
 			$this->fragment = isset($uriParts['fragment']) ? $uriParts['fragment'] : NULL;
+		} else {
+			throw new \InvalidArgumentException('The given URI "' . $uriString . '" is not a valid one.', 1351594202, $parseUrlException);
 		}
 	}
 

neos/flow

Original-Commit-Hash: 8d0010f29344bb351d28af24b60d05fc3b997b69

Classes/Http/Uri.php
@@ -93,7 +93,12 @@ class Uri {
 	public function __construct($uriString) {
 		if (!is_string($uriString)) throw new \InvalidArgumentException('The URI must be a valid string.', 1176550571);
 
-		$uriParts = parse_url($uriString);
+		$parseUrlException = NULL;
+		try {
+			$uriParts = parse_url($uriString);
+		} catch (\TYPO3\FLOW3\Error\Exception $exception) {
+			$parseUrlException = $exception;
+		}
 		if (is_array($uriParts)) {
 			$this->scheme = isset($uriParts['scheme']) ? $uriParts['scheme'] : NULL;
 			$this->username = isset($uriParts['user']) ? $uriParts['user'] : NULL;
@@ -105,6 +110,8 @@ class Uri {
 				$this->setQuery ($uriParts['query']);
 			}
 			$this->fragment = isset($uriParts['fragment']) ? $uriParts['fragment'] : NULL;
+		} else {
+			throw new \InvalidArgumentException('The given URI "' . $uriString . '" is not a valid one.', 1351594202, $parseUrlException);
 		}
 	}
 