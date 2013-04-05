neos/flow-development-collection

[BUGFIX] Flow should work with PHP versions lower than 5.3.9
The change Ia42e374202845dfe5e95c72512e9fefb4f00e789 introduced
usage of is_a() with 3 arguments which is only implemented from
PHP 5.3.9. This change replaces the call with a manual check for
inheritance.

Change-Id: I05c42b785e3f49e974cd5b381940e96130e5b0dc
Related: #32829
Releases: master
Original-Commit-Hash: 45ea227709db707db3165e4b46c68834e8b671d8

TYPO3.Flow/Classes/TYPO3/Flow/Property/TypeConverter/ObjectConverter.php
@@ -186,8 +186,14 @@ class ObjectConverter extends AbstractTypeConverter {
 			if ($configuration->getConfigurationValue('TYPO3\Flow\Property\TypeConverter\ObjectConverter', self::CONFIGURATION_OVERRIDE_TARGET_TYPE_ALLOWED) !== TRUE) {
 				throw new \TYPO3\Flow\Property\Exception\InvalidPropertyMappingConfigurationException('Override of target type not allowed. To enable this, you need to set the PropertyMappingConfiguration Value "CONFIGURATION_OVERRIDE_TARGET_TYPE_ALLOWED" to TRUE.', 1317050430);
 			}
-			if ($targetType !== $originalTargetType && is_a($targetType, $originalTargetType, TRUE) === FALSE) {
-				throw new \TYPO3\Flow\Property\Exception\InvalidDataTypeException('The given type "' . $targetType . '" is not a subtype of "' . $originalTargetType . '".', 1317048056);
+
+				// FIXME: The following check and the checkInheritanceChainWithoutIsA() method should be removed if we raise the PHP requirement to 5.3.9 or higher
+			if (version_compare(phpversion(), '5.3.8', '>')) {
+				if ($targetType !== $originalTargetType && is_a($targetType, $originalTargetType, TRUE) === FALSE) {
+					throw new \TYPO3\Flow\Property\Exception\InvalidDataTypeException('The given type "' . $targetType . '" is not a subtype of "' . $originalTargetType . '".', 1317048056);
+				}
+			} else {
+				$targetType = $this->checkInheritanceChainWithoutIsA($targetType, $originalTargetType);
 			}
 		}
 
@@ -226,6 +232,26 @@ class ObjectConverter extends AbstractTypeConverter {
 		} else {
 			return new $className();
 		}
+	}
+
+	/**
+	 * This is a replacement for the functionality provided by is_a() with 3 parameters which is only available from
+	 * PHP 5.3.9. It can be removed if the TYPO3.Flow PHP version requirement is raised to 5.3.9 or above.
+	 *
+	 * @param string $targetType
+	 * @param string $originalTargetType
+	 * @return string
+	 * @throws \TYPO3\Flow\Property\Exception\InvalidDataTypeException
+	 */
+	protected function checkInheritanceChainWithoutIsA($targetType, $originalTargetType) {
+		$targetTypeToCompare = $targetType;
+		do {
+			if ($targetTypeToCompare === $originalTargetType) {
+				return $targetType;
+			}
+		} while ($targetTypeToCompare = get_parent_class($targetTypeToCompare));
+
+		throw new \TYPO3\Flow\Property\Exception\InvalidDataTypeException('The given type "' . $targetType . '" is not a subtype of "' . $originalTargetType . '".', 1360928582);
 	}
 
 }

neos/flow

Releases: master, 2.0, 1.1
Original-Commit-Hash: 006c7eb9be2e61cef00353367ec9d7a802fbcd2f

Classes/Property/TypeConverter/ObjectConverter.php
@@ -180,8 +180,14 @@ class ObjectConverter extends \TYPO3\FLOW3\Property\TypeConverter\AbstractTypeCo
 			if ($configuration->getConfigurationValue('TYPO3\FLOW3\Property\TypeConverter\ObjectConverter', self::CONFIGURATION_OVERRIDE_TARGET_TYPE_ALLOWED) !== TRUE) {
 				throw new \TYPO3\FLOW3\Property\Exception\InvalidPropertyMappingConfigurationException('Override of target type not allowed. To enable this, you need to set the PropertyMappingConfiguration Value "CONFIGURATION_OVERRIDE_TARGET_TYPE_ALLOWED" to TRUE.', 1317050430);
 			}
-			if ($targetType !== $originalTargetType && is_a($targetType, $originalTargetType, TRUE) === FALSE) {
-				throw new \TYPO3\FLOW3\Property\Exception\InvalidDataTypeException('The given type "' . $targetType . '" is not a subtype of "' . $originalTargetType . '".', 1317048056);
+
+				// FIXME: The following check and the checkInheritanceChainWithoutIsA() method should be removed if we raise the PHP requirement to 5.3.9 or higher
+			if (version_compare(phpversion(), '5.3.8', '>')) {
+				if ($targetType !== $originalTargetType && is_a($targetType, $originalTargetType, TRUE) === FALSE) {
+					throw new \TYPO3\FLOW3\Property\Exception\InvalidDataTypeException('The given type "' . $targetType . '" is not a subtype of "' . $originalTargetType . '".', 1317048056);
+				}
+			} else {
+				$targetType = $this->checkInheritanceChainWithoutIsA($targetType, $originalTargetType);
 			}
 		}
 
@@ -220,6 +226,26 @@ class ObjectConverter extends \TYPO3\FLOW3\Property\TypeConverter\AbstractTypeCo
 		} else {
 			return new $className();
 		}
+	}
+
+	/**
+	 * This is a replacement for the functionality provided by is_a() with 3 parameters which is only available from
+	 * PHP 5.3.9. It can be removed if the TYPO3.Flow PHP version requirement is raised to 5.3.9 or above.
+	 *
+	 * @param string $targetType
+	 * @param string $originalTargetType
+	 * @return string
+	 * @throws \TYPO3\FLOW3\Property\Exception\InvalidDataTypeException
+	 */
+	protected function checkInheritanceChainWithoutIsA($targetType, $originalTargetType) {
+		$targetTypeToCompare = $targetType;
+		do {
+			if ($targetTypeToCompare === $originalTargetType) {
+				return $targetType;
+			}
+		} while ($targetTypeToCompare = get_parent_class($targetTypeToCompare));
+
+		throw new \TYPO3\FLOW3\Property\Exception\InvalidDataTypeException('The given type "' . $targetType . '" is not a subtype of "' . $originalTargetType . '".', 1360928582);
 	}
 
 }