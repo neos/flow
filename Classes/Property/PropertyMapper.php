neos/flow-development-collection

[BUGFIX] PropertyMapper now normalizes simple types
This fixes a bug where the property mapper was unable to find a
valid typeconverter for properties annotated with int or bool.

Change-Id: Ic9db9626613083bc8400ca3d96ca85475c7fb106
Fixes: #47054
Releases: master, 2.0, 1.1
Original-Commit-Hash: 8eca52b6c44787b60a210dec1c653ca532326aff

TYPO3.Flow/Classes/TYPO3/Flow/Property/PropertyMapper.php
@@ -12,6 +12,7 @@ namespace TYPO3\Flow\Property;
  *                                                                        */
 
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Utility\TypeHandling;
 
 /**
  * The Property Mapper transforms simple types (arrays, strings, integers, floats, booleans) to objects or other simple types.
@@ -203,18 +204,19 @@ class PropertyMapper {
 			throw new \TYPO3\Flow\Property\Exception\InvalidTargetException('The target type was no string, but of type "' . gettype($targetType) . '"', 1297941727);
 		}
 		$targetType = $this->parseCompositeType($targetType);
+		$normalizedTargetType = TypeHandling::normalizeType($targetType);
 		$converter = NULL;
 
-		if (\TYPO3\Flow\Utility\TypeHandling::isSimpleType($targetType)) {
-			if (isset($this->typeConverters[$sourceType][$targetType])) {
-				$converter = $this->findEligibleConverterWithHighestPriority($this->typeConverters[$sourceType][$targetType], $source, $targetType);
+		if (TypeHandling::isSimpleType($normalizedTargetType)) {
+			if (isset($this->typeConverters[$sourceType][$normalizedTargetType])) {
+				$converter = $this->findEligibleConverterWithHighestPriority($this->typeConverters[$sourceType][$normalizedTargetType], $source, $normalizedTargetType);
 			}
 		} else {
-			$converter = $this->findFirstEligibleTypeConverterInObjectHierarchy($source, $sourceType, $targetType);
+			$converter = $this->findFirstEligibleTypeConverterInObjectHierarchy($source, $sourceType, $normalizedTargetType);
 		}
 
 		if ($converter === NULL) {
-			throw new \TYPO3\Flow\Property\Exception\TypeConverterException('No converter found which can be used to convert from "' . $sourceType . '" to "' . $targetType . '".');
+			throw new \TYPO3\Flow\Property\Exception\TypeConverterException('No converter found which can be used to convert from "' . $sourceType . '" to "' . $normalizedTargetType . '".');
 		}
 
 		return $converter;

neos/flow

Original-Commit-Hash: 519bfcc473de9980cd4d0499b285ed1fb9c73425

Classes/Property/PropertyMapper.php
@@ -12,6 +12,7 @@ namespace TYPO3\FLOW3\Property;
  *                                                                        */
 
 use TYPO3\FLOW3\Annotations as FLOW3;
+use TYPO3\FLOW3\Utility\TypeHandling;
 
 /**
  * The Property Mapper transforms simple types (arrays, strings, integers, floats, booleans) to objects or other simple types.
@@ -215,18 +216,19 @@ class PropertyMapper {
 			throw new \TYPO3\FLOW3\Property\Exception\InvalidTargetException('The target type was no string, but of type "' . gettype($targetType) . '"', 1297941727);
 		}
 		$targetType = $this->parseCompositeType($targetType);
+		$normalizedTargetType = TypeHandling::normalizeType($targetType);
 		$converter = NULL;
 
-		if (\TYPO3\FLOW3\Utility\TypeHandling::isSimpleType($targetType)) {
-			if (isset($this->typeConverters[$sourceType][$targetType])) {
-				$converter = $this->findEligibleConverterWithHighestPriority($this->typeConverters[$sourceType][$targetType], $source, $targetType);
+		if (TypeHandling::isSimpleType($normalizedTargetType)) {
+			if (isset($this->typeConverters[$sourceType][$normalizedTargetType])) {
+				$converter = $this->findEligibleConverterWithHighestPriority($this->typeConverters[$sourceType][$normalizedTargetType], $source, $normalizedTargetType);
 			}
 		} else {
-			$converter = $this->findFirstEligibleTypeConverterInObjectHierarchy($source, $sourceType, $targetType);
+			$converter = $this->findFirstEligibleTypeConverterInObjectHierarchy($source, $sourceType, $normalizedTargetType);
 		}
 
 		if ($converter === NULL) {
-			throw new \TYPO3\FLOW3\Property\Exception\TypeConverterException('No converter found which can be used to convert from "' . $sourceType . '" to "' . $targetType . '".');
+			throw new \TYPO3\FLOW3\Property\Exception\TypeConverterException('No converter found which can be used to convert from "' . $sourceType . '" to "' . $normalizedTargetType . '".');
 		}
 
 		return $converter;