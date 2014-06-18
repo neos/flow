neos/flow-development-collection

[BUGFIX] SplitSources break on glob errors
When using SplitSource a glob call is used to fetch all
configuration files. This can return FALSE on error and on some
systems also on no result which was not correctly catched.

Change-Id: I5276e783048fb361c0d109d6c99821735b1f68a2
Releases: master, 2.2, 2.1
Original-Commit-Hash: 807591898763c20c3b04bdfcbb3d3d06a017a755

TYPO3.Flow/Classes/TYPO3/Flow/Configuration/Source/YamlSource.php
@@ -48,9 +48,11 @@ class YamlSource {
 	public function has($pathAndFilename, $allowSplitSource = FALSE) {
 		if ($allowSplitSource === TRUE) {
 			$pathsAndFileNames = glob($pathAndFilename . '.*.yaml');
-			foreach ($pathsAndFileNames as $pathAndFilename) {
-				if (file_exists($pathAndFilename)) {
-					return TRUE;
+			if ($pathsAndFileNames !== FALSE) {
+				foreach ($pathsAndFileNames as $pathAndFilename) {
+					if (file_exists($pathAndFilename)) {
+						return TRUE;
+					}
 				}
 			}
 		}
@@ -74,8 +76,10 @@ class YamlSource {
 		$pathsAndFileNames = array($pathAndFilename . '.yaml');
 		if ($allowSplitSource === TRUE) {
 			$splitSourcePathsAndFileNames = glob($pathAndFilename . '.*.yaml');
-			sort($splitSourcePathsAndFileNames);
-			$pathsAndFileNames = array_merge($pathsAndFileNames, $splitSourcePathsAndFileNames);
+			if ($splitSourcePathsAndFileNames !== FALSE) {
+				sort($splitSourcePathsAndFileNames);
+				$pathsAndFileNames = array_merge($pathsAndFileNames, $splitSourcePathsAndFileNames);
+			}
 		}
 		$configuration = array();
 		foreach ($pathsAndFileNames as $pathAndFilename) {

neos/flow

Original-Commit-Hash: c7243360e2a7a47a9e54102f05b1caf52a9ce38d

Classes/TYPO3/Flow/Configuration/Source/YamlSource.php
@@ -33,9 +33,11 @@ class YamlSource {
 	public function has($pathAndFilename, $allowSplitSource = FALSE) {
 		if ($allowSplitSource === TRUE) {
 			$pathsAndFileNames = glob($pathAndFilename . '*.yaml');
-			foreach ($pathsAndFileNames as $pathAndFilename) {
-				if (file_exists($pathAndFilename)) {
-					return TRUE;
+			if ($pathsAndFileNames !== FALSE) {
+				foreach ($pathsAndFileNames as $pathAndFilename) {
+					if (file_exists($pathAndFilename)) {
+						return TRUE;
+					}
 				}
 			}
 		} else {
@@ -57,11 +59,13 @@ class YamlSource {
 	 * @throws \TYPO3\Flow\Configuration\Exception\ParseErrorException
 	 */
 	public function load($pathAndFilename, $allowSplitSource = FALSE) {
+		$pathsAndFileNames = array($pathAndFilename . '.yaml');
 		if ($allowSplitSource === TRUE) {
-			$pathsAndFileNames = glob($pathAndFilename . '.*.yaml');
-			sort($pathsAndFileNames);
-		} else {
-			$pathsAndFileNames = array($pathAndFilename . '.yaml');
+			$splitSourcePathsAndFileNames = glob($pathAndFilename . '.*.yaml');
+			if ($splitSourcePathsAndFileNames !== FALSE) {
+				$pathsAndFileNames = $splitSourcePathsAndFileNames;
+				sort($pathsAndFileNames);
+			}
 		}
 		$configuration = array();
 		foreach ($pathsAndFileNames as $pathAndFilename) {