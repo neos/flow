mneuhaus/TYPO3.Flow

[BUGFIX] Controller changes trigger AOP class rebuild
This is a pragmatic fix to avoid clearing all caches when
you add an action to a protected controller. Controllers will now
be watched by the FileMonitor to trigger rebuilding the policy
information.

Change-Id: I511464682f0ef5adcfd019899128923af8f70a80
Releases: master, 2.2, 2.1

Classes/TYPO3/Flow/Cache/CacheManager.php
@@ -228,12 +228,16 @@ class CacheManager {
 			}
 		}
 		$flushDoctrineProxyCache = FALSE;
+		$flushPolicyCache = FALSE;
 		if (count($modifiedClassNamesWithUnderscores) > 0) {
 			$reflectionStatusCache = $this->getCache('Flow_Reflection_Status');
 			foreach (array_keys($modifiedClassNamesWithUnderscores) as $classNameWithUnderscores) {
 				$reflectionStatusCache->remove($classNameWithUnderscores);
 				if ($flushDoctrineProxyCache === FALSE && preg_match('/_Domain_Model_(.+)/', $classNameWithUnderscores) === 1) {
 					$flushDoctrineProxyCache = TRUE;
+				}
+				if ($flushPolicyCache === FALSE && preg_match('/_Controller_(.+)Controller/', $classNameWithUnderscores) === 1) {
+					$flushPolicyCache = TRUE;
 				}
 			}
 			$objectConfigurationCache->remove('allCompiledCodeUpToDate');
@@ -246,6 +250,12 @@ class CacheManager {
 			$this->systemLogger->log('Domain model changes have been detected, triggering Doctrine 2 proxy rebuilding.', LOG_INFO);
 			$this->getCache('Flow_Persistence_Doctrine')->flush();
 			$objectConfigurationCache->remove('doctrineProxyCodeUpToDate');
+		}
+		if ($flushPolicyCache === TRUE) {
+			$this->systemLogger->log('Controller changes have been detected, trigger AOP rebuild.', LOG_INFO);
+			$this->getCache('Flow_Security_Policy')->flush();
+			$objectConfigurationCache->remove('allAspectClassesUpToDate');
+			$objectConfigurationCache->remove('allCompiledCodeUpToDate');
 		}
 	}
 

neos/flow

Releases: master, 3.0
Original-Commit-Hash: f326e5662bd87d1d3394c6e7615792481926db55

Classes/TYPO3/Flow/Cache/CacheManager.php
@@ -297,12 +297,16 @@ class CacheManager {
 			}
 		}
 		$flushDoctrineProxyCache = FALSE;
+		$flushPolicyCache = FALSE;
 		if (count($modifiedClassNamesWithUnderscores) > 0) {
 			$reflectionStatusCache = $this->getCache('Flow_Reflection_Status');
 			foreach (array_keys($modifiedClassNamesWithUnderscores) as $classNameWithUnderscores) {
 				$reflectionStatusCache->remove($classNameWithUnderscores);
 				if ($flushDoctrineProxyCache === FALSE && preg_match('/_Domain_Model_(.+)/', $classNameWithUnderscores) === 1) {
 					$flushDoctrineProxyCache = TRUE;
+				}
+				if ($flushPolicyCache === FALSE && preg_match('/_Controller_(.+)Controller/', $classNameWithUnderscores) === 1) {
+					$flushPolicyCache = TRUE;
 				}
 			}
 			$objectConfigurationCache->remove('allCompiledCodeUpToDate');
@@ -315,6 +319,12 @@ class CacheManager {
 			$this->systemLogger->log('Domain model changes have been detected, triggering Doctrine 2 proxy rebuilding.', LOG_INFO);
 			$this->getCache('Flow_Persistence_Doctrine')->flush();
 			$objectConfigurationCache->remove('doctrineProxyCodeUpToDate');
+		}
+		if ($flushPolicyCache === TRUE) {
+			$this->systemLogger->log('Controller changes have been detected, trigger AOP rebuild.', LOG_INFO);
+			$this->getCache('Flow_Security_Authorization_Privilege_Method')->flush();
+			$objectConfigurationCache->remove('allAspectClassesUpToDate');
+			$objectConfigurationCache->remove('allCompiledCodeUpToDate');
 		}
 	}
 