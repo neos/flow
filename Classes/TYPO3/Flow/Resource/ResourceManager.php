neos/flow

commit cbb0732d621d24e89b9d9f31acdc45ea346d432f

[FEATURE] Configurable extension blacklist for file uploads
The ResourceManager now has a blacklist for file extension that must not
be uploaded.

It defaults to a number of popular script language file extensions.

Fixes FLOW-408.

Classes/TYPO3/Flow/Resource/ResourceManager.php
@@ -19,7 +19,6 @@ use TYPO3\Flow\Persistence\PersistenceManagerInterface;
 use TYPO3\Flow\Reflection\ObjectAccess;
 use TYPO3\Flow\Resource\Storage\StorageInterface;
 use TYPO3\Flow\Resource\Storage\WritableStorageInterface;
-use TYPO3\Flow\Resource\Streams\StreamWrapperAdapter;
 use TYPO3\Flow\Resource\Target\TargetInterface;
 use TYPO3\Flow\Utility\Unicode\Functions as UnicodeFunctions;
 
@@ -208,7 +207,7 @@ class ResourceManager
      *
      * @param array $uploadInfo An array detailing the resource to import (expected keys: name, tmp_name)
      * @param string $collectionName Name of the collection this uploaded resource should be added to
-     * @return Resource A resource object representing the imported resource or FALSE if an error occurred.
+     * @return Resource A resource object representing the imported resource
      * @throws Exception
      */
     public function importUploadedResource(array $uploadInfo, $collectionName = self::DEFAULT_PERSISTENT_COLLECTION_NAME)
@@ -558,6 +557,10 @@ class ResourceManager
 
         if (!is_uploaded_file($temporaryTargetPathAndFilename)) {
             throw new Exception('The given upload file "' . strip_tags($pathInfo['basename']) . '" was not uploaded through PHP. As it could pose a security risk it cannot be imported.', 1422461503);
+        }
+
+        if (isset($pathInfo['extension']) && array_key_exists(strtolower($pathInfo['extension']), $this->settings['resource']['uploadExtensionBlacklist']) && $this->settings['resource']['uploadExtensionBlacklist'][strtolower($pathInfo['extension'])] === true) {
+            throw new Exception('The extension of the given upload file "' . strip_tags($pathInfo['basename']) . '" is blacklisted. As it could pose a security risk it cannot be imported.', 1447148472);
         }
 
         if ($openBasedirEnabled === true) {

Classes/TYPO3/Flow/Resource/ResourceTypeConverter.php
@@ -185,12 +185,14 @@ class ResourceTypeConverter extends AbstractTypeConverter
             return $this->convertedResources[$source['tmp_name']];
         }
 
-        $resource = $this->resourceManager->importUploadedResource($source, $this->getCollectionName($source, $configuration));
-        if ($resource === false) {
-            return new Error('The Resource Manager could not create a Resource instance for an uploaded file. See log for more details.', 1264517906);
-        } else {
+        try {
+            $resource = $this->resourceManager->importUploadedResource($source, $this->getCollectionName($source, $configuration));
             $this->convertedResources[$source['tmp_name']] = $resource;
             return $resource;
+        } catch (\Exception $exception) {
+            $this->systemLogger->log('Could not import an uploaded file', LOG_WARNING);
+            $this->systemLogger->logException($exception);
+            return new Error('During import of an uploaded file an error occurred. See log for more details.', 1264517906);
         }
     }
 

Configuration/Settings.yaml
@@ -319,6 +319,22 @@ TYPO3:
 
     resource:
 
+      # A list of filename extensions that must not be uploaded through the resource
+      # management.
+      uploadExtensionBlacklist:
+        'aspx': TRUE
+        'cgi': TRUE
+        'php3': TRUE
+        'php4': TRUE
+        'php5': TRUE
+        'phtml': TRUE
+        'php': TRUE
+        'pl': TRUE
+        'py': TRUE
+        'pyc': TRUE
+        'pyo': TRUE
+        'rb': TRUE
+
       # Definition of resource storages provided by the Flow core.
       # Storages are used for storing and retrieving resources.
       storages:

Resources/Private/Schema/Settings/TYPO3.Flow.resource.schema.yaml
@@ -1,6 +1,10 @@
 type: dictionary
 additionalProperties: FALSE
 properties:
+  'uploadExtensionBlacklist':
+    type: dictionary
+    additionalProperties:
+      type: boolean
   'storages':
     type: dictionary
     required: TRUE

Tests/Unit/Resource/ResourceTypeConverterTest.php
@@ -19,6 +19,7 @@ use TYPO3\Flow\Tests\UnitTestCase;
  */
 class ResourceTypeConverterTest extends UnitTestCase
 {
+
     /**
      * @var \TYPO3\Flow\Resource\ResourceTypeConverter
      */
@@ -118,7 +119,7 @@ class ResourceTypeConverterTest extends UnitTestCase
         $source = array(
             'error' => \UPLOAD_ERR_NO_FILE,
             'originallySubmittedResource' => array(
-                    '__identity' => '79ecda60-1a27-69ca-17bf-a5d9e80e6c39'
+                '__identity' => '79ecda60-1a27-69ca-17bf-a5d9e80e6c39'
             )
         );
 
@@ -159,7 +160,6 @@ class ResourceTypeConverterTest extends UnitTestCase
         $this->resourceTypeConverter->convertFrom($source, 'TYPO3\Flow\Resource\Resource');
     }
 
-
     /**
      * @test
      */
@@ -181,11 +181,13 @@ class ResourceTypeConverterTest extends UnitTestCase
      */
     public function convertFromReturnsAnErrorIfTheUploadedFileCantBeImported()
     {
+        $this->inject($this->resourceTypeConverter, 'systemLogger', $this->getMock(\TYPO3\Flow\Log\SystemLoggerInterface::class));
+
         $source = array(
             'tmp_name' => 'SomeFilename',
             'error' => \UPLOAD_ERR_OK
         );
-        $this->mockResourceManager->expects($this->once())->method('importUploadedResource')->with($source)->will($this->returnValue(false));
+        $this->mockResourceManager->expects($this->once())->method('importUploadedResource')->with($source)->will($this->throwException(new \TYPO3\Flow\Resource\Exception()));
 
         $actualResult = $this->resourceTypeConverter->convertFrom($source, 'TYPO3\Flow\Resource\Resource');
         $this->assertInstanceOf('TYPO3\Flow\Error\Error', $actualResult);

commit fa692e3eab733c8fb7950d5f36e2fc194b5c4d8c

[BUGFIX] Add missing use statement
A regression from cbb0732d621d24e89b9d9f31acdc45ea346d432f

Classes/TYPO3/Flow/Resource/ResourceManager.php
@@ -19,6 +19,7 @@ use TYPO3\Flow\Persistence\PersistenceManagerInterface;
 use TYPO3\Flow\Reflection\ObjectAccess;
 use TYPO3\Flow\Resource\Storage\StorageInterface;
 use TYPO3\Flow\Resource\Storage\WritableStorageInterface;
+use TYPO3\Flow\Resource\Streams\StreamWrapperAdapter;
 use TYPO3\Flow\Resource\Target\TargetInterface;
 use TYPO3\Flow\Utility\Unicode\Functions as UnicodeFunctions;
 