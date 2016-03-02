neos/flow

commit 35bbb3166f8b73b4c1fefff250b21dfdb9b55134

TASK: Do not initialize ReflectionService on shutdown if not needed
The ReflectionService would be still initialized before having an early
return in the saveToCache() method. This change makes sure to not
initialize the ReflectionService which results in less cache entries
being loaded and therefore less memory consumption and I/O.

NEOS-571 #comment Optimize ReflectionService shutdown

Classes/TYPO3/Flow/Reflection/ReflectionService.php
@@ -341,7 +341,7 @@ class ReflectionService
     {
         $this->context = $this->environment->getContext();
 
-        if ($this->context->isProduction() && $this->reflectionDataRuntimeCache->getBackend()->isFrozen()) {
+        if ($this->hasFrozenCacheInProduction()) {
             $this->classReflectionData = $this->reflectionDataRuntimeCache->get('__classNames');
             $this->annotatedClasses = $this->reflectionDataRuntimeCache->get('__annotatedClasses');
             $this->loadFromClassSchemaRuntimeCache = true;
@@ -2043,6 +2043,9 @@ class ReflectionService
      */
     public function saveToCache()
     {
+        if ($this->hasFrozenCacheInProduction()) {
+            return;
+        }
         if (!$this->initialized) {
             $this->initialize();
         }
@@ -2176,5 +2179,13 @@ class ReflectionService
     protected function getPrecompiledReflectionStoragePath()
     {
         return Files::concatenatePaths(array($this->environment->getPathToTemporaryDirectory(), 'PrecompiledReflectionData/')) . '/';
+    }
+
+    /**
+     * @return boolean
+     */
+    protected function hasFrozenCacheInProduction()
+    {
+        return $this->context->isProduction() && $this->reflectionDataRuntimeCache->getBackend()->isFrozen();
     }
 }

commit ae5d83d6f8712cb14f3e72da7b485c0ae3e131ca

BUGFIX: Fix regression with frozen reflection service
The commit 35bbb3166f8b73b4c1fefff250b21dfdb9b55134 introduced a change
for early return inside the reflection service that did not catch a case
where the context was not initialized from the environment.

Classes/TYPO3/Flow/Reflection/ReflectionService.php
@@ -2186,6 +2186,6 @@ class ReflectionService
      */
     protected function hasFrozenCacheInProduction()
     {
-        return $this->context->isProduction() && $this->reflectionDataRuntimeCache->getBackend()->isFrozen();
+        return $this->environment->getContext()->isProduction() && $this->reflectionDataRuntimeCache->getBackend()->isFrozen();
     }
 }