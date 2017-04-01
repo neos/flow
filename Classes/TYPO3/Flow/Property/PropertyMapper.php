neos/flow

commit d4a97ae393f71fd261ac83b909ae830577044f96

TASK: Update references

Classes/TYPO3/Flow/Property/PropertyMapper.php
@@ -403,8 +403,8 @@ class PropertyMapper
         return $typeConverterMap;
     }
 
-    /**
-     * Returns a multi-dimensional array with the Type Converters available in the system.
+/**
+ * Returns a multi-dimensional array with the Type Converters available in the system.
      *
      * It has the following structure:
      *

commit 138995f0a6a2fedb0377040e4a0764cb4e133a1e

BUGFIX: Revert "TASK: Update references"
This reverts commit d4a97ae393f71fd261ac83b909ae830577044f96,
caused by a bug in the reference update script.

See neos/flow-development-distribution#24

Classes/TYPO3/Flow/Property/PropertyMapper.php
@@ -403,8 +403,8 @@ class PropertyMapper
         return $typeConverterMap;
     }
 
-/**
- * Returns a multi-dimensional array with the Type Converters available in the system.
+    /**
+     * Returns a multi-dimensional array with the Type Converters available in the system.
      *
      * It has the following structure:
      *