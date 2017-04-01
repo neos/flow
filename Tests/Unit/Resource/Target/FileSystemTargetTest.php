neos/flow

commit 1c8c8ff7ffa484bfa26f2ad9b41d544f746e023f

TASK: Update references

Classes/TYPO3/Flow/Resource/Collection.php
@@ -158,9 +158,8 @@ class Collection implements CollectionInterface
     {
         $objects = [];
         if ($this->storage instanceof PackageStorage && $this->pathPatterns !== []) {
-            $objects = new \AppendIterator();
             foreach ($this->pathPatterns as $pathPattern) {
-                $objects->append($this->storage->getObjectsByPathPattern($pathPattern, $callback));
+                $objects = array_merge($objects, $this->storage->getObjectsByPathPattern($pathPattern, $callback));
             }
         } else {
             $objects = $this->storage->getObjectsByCollection($this, $callback);

Tests/Unit/Resource/Target/FileSystemTargetTest.php
@@ -11,21 +11,15 @@ namespace TYPO3\Flow\Tests\Unit\Resource\Streams;
  * source code.
  */
 
-use TYPO3\Flow\Package;
 use TYPO3\Flow\Cli\CommandRequestHandler;
 use TYPO3\Flow\Core\Bootstrap;
 use TYPO3\Flow\Core\RequestHandlerInterface;
 use TYPO3\Flow\Http\HttpRequestHandlerInterface;
 use TYPO3\Flow\Http\Request;
 use TYPO3\Flow\Http\Uri;
-use TYPO3\Flow\Log\SystemLoggerInterface;
-use TYPO3\Flow\Package\PackageManager;
 use TYPO3\Flow\Resource\Resource;
-use TYPO3\Flow\Resource\Collection;
-use TYPO3\Flow\Resource\Storage\PackageStorage;
 use TYPO3\Flow\Resource\Target\FileSystemTarget;
 use TYPO3\Flow\Tests\UnitTestCase;
-use org\bovigo\vfs\vfsStream;
 
 /**
  * Tests for the FileSystemTarget class
@@ -200,37 +194,5 @@ class FileSystemTargetTest extends UnitTestCase
         $mockResource = $this->getMockBuilder(Resource::class)->disableOriginalConstructor()->getMock();
 
         $this->fileSystemTarget->getPublicStaticResourceUri($mockResource);
-    }
-
-    /**
-     * @test
-     */
-    public function getWorksWithPackageStorage()
-    {
-        vfsStream::setup('Test');
-        mkdir('vfs://Test/Configuration');
-        $packageManager = new PackageManager('vfs://Test/Configuration/PackageStates.php');
-        $packageManager->createPackage("TYPO3.Flow");
-        $packageManager->activatePackage("TYPO3.Flow");
-
-        $packageStorage = new PackageStorage('testStorage');
-
-        $mockSystemLogger = $this->getMockBuilder(SystemLoggerInterface::class)->getMock();
-
-        $this->inject($packageStorage, 'packageManager', $packageManager);
-
-        $oneResourcePublished = false;
-
-        $_publicationCallback = function ($i, $o) use (&$oneResourcePublished) {
-            $oneResourcePublished = true;
-        };
-
-        $staticCollection = new Collection('testStaticCollection', $packageStorage, $this->fileSystemTarget, ['*']);
-
-        $fileSystemTarget = new FileSystemTarget('test', ['path' => 'vfs://Publish']);
-        $this->inject($fileSystemTarget, 'systemLogger', $mockSystemLogger);
-        $fileSystemTarget->publishCollection($staticCollection, $_publicationCallback);
-
-        $this->assertTrue($oneResourcePublished);
     }
 }

commit 1bf91d0ffe2e0d0c7889e17f4447c5457dfb826d

BUGFIX: Revert "TASK: Update references"
This reverts commit 1c8c8ff7ffa484bfa26f2ad9b41d544f746e023f,
caused by a bug in the reference update script.

See neos/flow-development-distribution#24

Classes/TYPO3/Flow/Resource/Collection.php
@@ -158,8 +158,9 @@ class Collection implements CollectionInterface
     {
         $objects = [];
         if ($this->storage instanceof PackageStorage && $this->pathPatterns !== []) {
+            $objects = new \AppendIterator();
             foreach ($this->pathPatterns as $pathPattern) {
-                $objects = array_merge($objects, $this->storage->getObjectsByPathPattern($pathPattern, $callback));
+                $objects->append($this->storage->getObjectsByPathPattern($pathPattern, $callback));
             }
         } else {
             $objects = $this->storage->getObjectsByCollection($this, $callback);

Tests/Unit/Resource/Target/FileSystemTargetTest.php
@@ -11,15 +11,21 @@ namespace TYPO3\Flow\Tests\Unit\Resource\Streams;
  * source code.
  */
 
+use TYPO3\Flow\Package;
 use TYPO3\Flow\Cli\CommandRequestHandler;
 use TYPO3\Flow\Core\Bootstrap;
 use TYPO3\Flow\Core\RequestHandlerInterface;
 use TYPO3\Flow\Http\HttpRequestHandlerInterface;
 use TYPO3\Flow\Http\Request;
 use TYPO3\Flow\Http\Uri;
+use TYPO3\Flow\Log\SystemLoggerInterface;
+use TYPO3\Flow\Package\PackageManager;
 use TYPO3\Flow\Resource\Resource;
+use TYPO3\Flow\Resource\Collection;
+use TYPO3\Flow\Resource\Storage\PackageStorage;
 use TYPO3\Flow\Resource\Target\FileSystemTarget;
 use TYPO3\Flow\Tests\UnitTestCase;
+use org\bovigo\vfs\vfsStream;
 
 /**
  * Tests for the FileSystemTarget class
@@ -194,5 +200,37 @@ class FileSystemTargetTest extends UnitTestCase
         $mockResource = $this->getMockBuilder(Resource::class)->disableOriginalConstructor()->getMock();
 
         $this->fileSystemTarget->getPublicStaticResourceUri($mockResource);
+    }
+
+    /**
+     * @test
+     */
+    public function getWorksWithPackageStorage()
+    {
+        vfsStream::setup('Test');
+        mkdir('vfs://Test/Configuration');
+        $packageManager = new PackageManager('vfs://Test/Configuration/PackageStates.php');
+        $packageManager->createPackage("TYPO3.Flow");
+        $packageManager->activatePackage("TYPO3.Flow");
+
+        $packageStorage = new PackageStorage('testStorage');
+
+        $mockSystemLogger = $this->getMockBuilder(SystemLoggerInterface::class)->getMock();
+
+        $this->inject($packageStorage, 'packageManager', $packageManager);
+
+        $oneResourcePublished = false;
+
+        $_publicationCallback = function ($i, $o) use (&$oneResourcePublished) {
+            $oneResourcePublished = true;
+        };
+
+        $staticCollection = new Collection('testStaticCollection', $packageStorage, $this->fileSystemTarget, ['*']);
+
+        $fileSystemTarget = new FileSystemTarget('test', ['path' => 'vfs://Publish']);
+        $this->inject($fileSystemTarget, 'systemLogger', $mockSystemLogger);
+        $fileSystemTarget->publishCollection($staticCollection, $_publicationCallback);
+
+        $this->assertTrue($oneResourcePublished);
     }
 }