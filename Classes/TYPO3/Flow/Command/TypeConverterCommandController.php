neos/flow

commit b6fa929f2a7a0f49872a8c7947ef662b2522d2b6

TASK: Update references

Classes/TYPO3/Flow/Command/TypeConverterCommandController.php
@@ -38,16 +38,16 @@ class TypeConverterCommandController extends CommandController
      */
     public function listCommand()
     {
-        foreach ($this->propertyMapper->getTypeConverters() as $sourceType => $targetTypePriorityAndClassName) {
+        foreach ($this->propertyMapper->getTypeConverters() as $sourceType => $targetTypePriorityAndInstance) {
             $this->outputLine();
             $this->outputLine('<b>Source type "%s":</b>', [$sourceType]);
 
-            foreach ($targetTypePriorityAndClassName as $targetType => $priorityAndClassName) {
+            foreach ($targetTypePriorityAndInstance as $targetType => $priorityAndInstance) {
                 $this->outputFormatted('<b>Target type "%s":</b>', [$targetType], 4);
 
-                krsort($priorityAndClassName);
-                foreach ($priorityAndClassName as $priority => $className) {
-                    $this->outputFormatted('%3s: %s', [$priority, $className], 8);
+                krsort($priorityAndInstance);
+                foreach ($priorityAndInstance as $priority => $instance) {
+                    $this->outputFormatted('%3s: %s', [$priority, get_class($instance)], 8);
                 }
                 $this->outputLine();
             }

Classes/TYPO3/Flow/Property/PropertyMapper.php
@@ -394,7 +394,7 @@ class PropertyMapper
             $typeConverter = $this->objectManager->get($typeConverterClassName);
             foreach ($typeConverter->getSupportedSourceTypes() as $supportedSourceType) {
                 if (isset($typeConverterMap[$supportedSourceType][$typeConverter->getSupportedTargetType()][$typeConverter->getPriority()])) {
-                    throw new Exception\DuplicateTypeConverterException('There exist at least two converters which handle the conversion from "' . $supportedSourceType . '" to "' . $typeConverter->getSupportedTargetType() . '" with priority "' . $typeConverter->getPriority() . '": ' . $typeConverterMap[$supportedSourceType][$typeConverter->getSupportedTargetType()][$typeConverter->getPriority()] . ' and ' . get_class($typeConverter), 1297951378);
+                    throw new Exception\DuplicateTypeConverterException('There exist at least two converters which handle the conversion from "' . $supportedSourceType . '" to "' . $typeConverter->getSupportedTargetType() . '" with priority "' . $typeConverter->getPriority() . '": ' . get_class($this->typeConverters[$supportedSourceType][$typeConverter->getSupportedTargetType()][$typeConverter->getPriority()]) . ' and ' . get_class($typeConverter), 1297951378);
                 }
                 $typeConverterMap[$supportedSourceType][$typeConverter->getSupportedTargetType()][$typeConverter->getPriority()] = $typeConverterClassName;
             }
@@ -403,8 +403,8 @@ class PropertyMapper
         return $typeConverterMap;
     }
 
-    /**
-     * Returns a multi-dimensional array with the Type Converters available in the system.
+/**
+ * Returns a multi-dimensional array with the Type Converters available in the system.
      *
      * It has the following structure:
      *

Resources/Private/Cache/SQL/DDL.sql
@@ -6,7 +6,7 @@ CREATE TABLE "cache" (
   "context" VARCHAR(150) NOT NULL,
   "created" INTEGER UNSIGNED NOT NULL,
   "lifetime" INTEGER UNSIGNED DEFAULT '0' NOT NULL,
-  "content" MEDIUMTEXT,
+  "content" TEXT,
   PRIMARY KEY ("identifier", "cache", "context")
 );
 

commit e05619962abe46579dc03baf348bc2cf173327e1

BUGFIX: Revert "TASK: Update references"
This reverts commit b6fa929f2a7a0f49872a8c7947ef662b2522d2b6,
caused by a bug in the reference update script.

See neos/flow-development-distribution#24

Classes/TYPO3/Flow/Command/TypeConverterCommandController.php
@@ -38,16 +38,16 @@ class TypeConverterCommandController extends CommandController
      */
     public function listCommand()
     {
-        foreach ($this->propertyMapper->getTypeConverters() as $sourceType => $targetTypePriorityAndInstance) {
+        foreach ($this->propertyMapper->getTypeConverters() as $sourceType => $targetTypePriorityAndClassName) {
             $this->outputLine();
             $this->outputLine('<b>Source type "%s":</b>', [$sourceType]);
 
-            foreach ($targetTypePriorityAndInstance as $targetType => $priorityAndInstance) {
+            foreach ($targetTypePriorityAndClassName as $targetType => $priorityAndClassName) {
                 $this->outputFormatted('<b>Target type "%s":</b>', [$targetType], 4);
 
-                krsort($priorityAndInstance);
-                foreach ($priorityAndInstance as $priority => $instance) {
-                    $this->outputFormatted('%3s: %s', [$priority, get_class($instance)], 8);
+                krsort($priorityAndClassName);
+                foreach ($priorityAndClassName as $priority => $className) {
+                    $this->outputFormatted('%3s: %s', [$priority, $className], 8);
                 }
                 $this->outputLine();
             }

Classes/TYPO3/Flow/Property/PropertyMapper.php
@@ -394,7 +394,7 @@ class PropertyMapper
             $typeConverter = $this->objectManager->get($typeConverterClassName);
             foreach ($typeConverter->getSupportedSourceTypes() as $supportedSourceType) {
                 if (isset($typeConverterMap[$supportedSourceType][$typeConverter->getSupportedTargetType()][$typeConverter->getPriority()])) {
-                    throw new Exception\DuplicateTypeConverterException('There exist at least two converters which handle the conversion from "' . $supportedSourceType . '" to "' . $typeConverter->getSupportedTargetType() . '" with priority "' . $typeConverter->getPriority() . '": ' . get_class($this->typeConverters[$supportedSourceType][$typeConverter->getSupportedTargetType()][$typeConverter->getPriority()]) . ' and ' . get_class($typeConverter), 1297951378);
+                    throw new Exception\DuplicateTypeConverterException('There exist at least two converters which handle the conversion from "' . $supportedSourceType . '" to "' . $typeConverter->getSupportedTargetType() . '" with priority "' . $typeConverter->getPriority() . '": ' . $typeConverterMap[$supportedSourceType][$typeConverter->getSupportedTargetType()][$typeConverter->getPriority()] . ' and ' . get_class($typeConverter), 1297951378);
                 }
                 $typeConverterMap[$supportedSourceType][$typeConverter->getSupportedTargetType()][$typeConverter->getPriority()] = $typeConverterClassName;
             }
@@ -403,8 +403,8 @@ class PropertyMapper
         return $typeConverterMap;
     }
 
-/**
- * Returns a multi-dimensional array with the Type Converters available in the system.
+    /**
+     * Returns a multi-dimensional array with the Type Converters available in the system.
      *
      * It has the following structure:
      *

Resources/Private/Cache/SQL/DDL.sql
@@ -6,7 +6,7 @@ CREATE TABLE "cache" (
   "context" VARCHAR(150) NOT NULL,
   "created" INTEGER UNSIGNED NOT NULL,
   "lifetime" INTEGER UNSIGNED DEFAULT '0' NOT NULL,
-  "content" TEXT,
+  "content" MEDIUMTEXT,
   PRIMARY KEY ("identifier", "cache", "context")
 );
 