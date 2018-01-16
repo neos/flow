neos/flow

commit c623dbf7c5e8535908926b9d35d4557376176f34

!!! FEATURE: More extensible Routing (#1126)
An overhauled Router implementation that allows for more flexible
routing.

## Features:

### Routing Parameters

Routing `Parameters` can be defined globally (via HTTP component) in order
to allow custom RoutePart handler to react to influences that are outside of
the incoming URI path (example: The requested host name or scheme)

For a RoutePart handler to access the parameters they have to implement
the new `ParameterAwareRoutePartInterface`.
The `DynamicRoutePart` already implements the interface. For custom implementations
extending `DynamicRoutePart` the parameters will be accessible via `$this->parameters`.

### Extended URI matching

RoutePart handlers can now return an instance of `MatchResult` when mapping
incoming requests.
This allows the handler to specify *Tags*  to be associated with the route.

#### Example:

*Before:*

```php
protected function matchValue($value)
{
     // custom logic, returning false if the $value doesn't match
     $this->value = $matchedValue;
     return true;
}
```

*Now:*

```php
protected function matchValue($value)
{
     // custom logic, returning false if the $value doesn't match
     return new MatchResult($matchedValue, RouteTags::createFromTag('some-tag'));
}
```
*Note:* The RouteTags argument is optional
*Note:* For backwards compatibility the above still works as before

### Extended URI resolving

Similar to the above, RoutePart handlers can now specify tags for *resolved* routes
by returning an instance of `ResolveResult`.
This also allows RouteParts to specify `UriConstraints` that will affect the resolved
URI.
UriConstraints allow to pre-set the following attributes of the resulting URI:

* Scheme  (for example "https")
* Host (for example "www.somedomain.tld")
* Host prefix (for example "en.")
* Host suffix (for example "co.uk")
* Port (for example 443)
* Path (for example "some/path")

Classes/Mvc/Routing/Dto/ResolveContext.php
@@ -1,0 +1,98 @@
+<?php
+namespace Neos\Flow\Mvc\Routing\Dto;
+
+/*
+ * This file is part of the Neos.Flow package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Simple DTO wrapping the values required for a Router::resolve() call
+ *
+ * @Flow\Proxy(false)
+ */
+final class ResolveContext
+{
+
+    /**
+     * The currently requested URI, required to fill in parts of the result when resolving absolute URIs
+     *
+     * @var UriInterface
+     */
+    private $requestUri;
+
+    /**
+     * Route values to build the URI, for example ['@action' => 'index', 'someArgument' => 'foo', ...]
+     *
+     * @var array
+     */
+    private $routeValues;
+
+    /**
+     * Whether or not an absolute URI is to be returned
+     *
+     * @var bool
+     */
+    private $forceAbsoluteUri;
+
+    /**
+     * A prefix to be prepended to any resolved URI
+     *
+     * @var string
+     */
+    private $uriPathPrefix;
+
+    /**
+     * @param UriInterface $requestUri The currently requested URI, required to fill in parts of the result when resolving absolute URIs
+     * @param array $routeValues Route values to build the URI, for example ['@action' => 'index', 'someArgument' => 'foo', ...]
+     * @param bool $forceAbsoluteUri Whether or not an absolute URI is to be returned
+     * @param string $uriPathPrefix A prefix to be prepended to any resolved URI
+     */
+    public function __construct(UriInterface $requestUri, array $routeValues, bool $forceAbsoluteUri, string $uriPathPrefix = '')
+    {
+        $this->requestUri = $requestUri;
+        $this->routeValues = $routeValues;
+        $this->forceAbsoluteUri = $forceAbsoluteUri;
+        $this->uriPathPrefix = $uriPathPrefix;
+    }
+
+    /**
+     * @return UriInterface
+     */
+    public function getRequestUri(): UriInterface
+    {
+        return $this->requestUri;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRouteValues(): array
+    {
+        return $this->routeValues;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isForceAbsoluteUri(): bool
+    {
+        return $this->forceAbsoluteUri;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUriPathPrefix(): string
+    {
+        return $this->uriPathPrefix;
+    }
+}

Classes/Mvc/Routing/Router.php
@@ -13,10 +13,12 @@ namespace Neos\Flow\Mvc\Routing;
 
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Configuration\ConfigurationManager;
-use Neos\Flow\Http\Request;
 use Neos\Flow\Log\SystemLoggerInterface;
 use Neos\Flow\Mvc\Exception\InvalidRouteSetupException;
 use Neos\Flow\Mvc\Exception\NoMatchingRouteException;
+use Neos\Flow\Mvc\Routing\Dto\ResolveContext;
+use Neos\Flow\Mvc\Routing\Dto\RouteContext;
+use Psr\Http\Message\UriInterface;
 
 /**
  * The default web router
@@ -92,32 +94,32 @@ class Router implements RouterInterface
      * Returns the matchResults of the matching route or NULL if no matching
      * route could be found.
      *
-     * @param Request $httpRequest The web request to be analyzed. Will be modified by the router.
+     * @param RouteContext $routeContext The Route Context containing the current HTTP Request and, optional, Routing RouteParameters
      * @return array The results of the matching route or NULL if no route matched
+     * @throws NoMatchingRouteException if no route matched the given $routeContext
      */
-    public function route(Request $httpRequest)
+    public function route(RouteContext $routeContext): array
     {
-        $cachedMatchResults = $this->routerCachingService->getCachedMatchResults($httpRequest);
-        if ($cachedMatchResults !== false) {
-            return $cachedMatchResults;
-        }
         $this->lastMatchedRoute = null;
+        $cachedRouteResult = $this->routerCachingService->getCachedMatchResults($routeContext);
+        if ($cachedRouteResult !== false) {
+            return $cachedRouteResult;
+        }
         $this->createRoutesFromConfiguration();
+        $httpRequest = $routeContext->getHttpRequest();
 
         /** @var $route Route */
         foreach ($this->routes as $route) {
-            if ($route->matches($httpRequest) === true) {
+            if ($route->matches($routeContext) === true) {
                 $this->lastMatchedRoute = $route;
                 $matchResults = $route->getMatchResults();
-                if ($matchResults !== null) {
-                    $this->routerCachingService->storeMatchResults($httpRequest, $matchResults);
-                }
-                $this->systemLogger->log(sprintf('Router route(): Route "%s" matched the path "%s".', $route->getName(), $httpRequest->getRelativePath()), LOG_DEBUG);
+                $this->routerCachingService->storeMatchResults($routeContext, $matchResults, $route->getMatchedTags());
+                $this->systemLogger->log(sprintf('Router route(): Route "%s" matched the request "%s (%s)".', $route->getName(), $httpRequest->getUri(), $httpRequest->getMethod()), LOG_DEBUG);
                 return $matchResults;
             }
         }
         $this->systemLogger->log(sprintf('Router route(): No route matched the route path "%s".', $httpRequest->getRelativePath()), LOG_NOTICE);
-        return null;
+        throw new NoMatchingRouteException('Could not match a route for the HTTP request.', 1510846308);
     }
 
     /**
@@ -160,32 +162,31 @@ class Router implements RouterInterface
      * method. If no matching route is found, an empty string is returned.
      * Note: calls of this message are cached by RouterCachingAspect
      *
-     * @param array $routeValues Key/value pairs to be resolved. E.g. array('@package' => 'MyPackage', '@controller' => 'MyController');
-     * @return string
-     * @throws NoMatchingRouteException
+     * @param ResolveContext $resolveContext The Resolve Context containing the route values, the request URI and some flags to be resolved
+     * @return UriInterface The resolved URI
+     * @throws NoMatchingRouteException if no route could resolve the given $resolveContext
      */
-    public function resolve(array $routeValues)
+    public function resolve(ResolveContext $resolveContext): UriInterface
     {
-        $cachedResolvedUriPath = $this->routerCachingService->getCachedResolvedUriPath($routeValues);
-        if ($cachedResolvedUriPath !== false) {
-            return $cachedResolvedUriPath;
+        $this->lastResolvedRoute = null;
+        $cachedResolvedUriConstraints = $this->routerCachingService->getCachedResolvedUriConstraints($resolveContext);
+        if ($cachedResolvedUriConstraints !== false) {
+            return $cachedResolvedUriConstraints->applyTo($resolveContext->getRequestUri(), $resolveContext->isForceAbsoluteUri());
         }
 
-        $this->lastResolvedRoute = null;
         $this->createRoutesFromConfiguration();
 
         /** @var $route Route */
         foreach ($this->routes as $route) {
-            if ($route->resolves($routeValues)) {
+            if ($route->resolves($resolveContext->getRouteValues()) === true) {
+                $uriConstraints = $route->getResolvedUriConstraints()->withPathPrefix($resolveContext->getUriPathPrefix());
+                $resolvedUri = $uriConstraints->applyTo($resolveContext->getRequestUri(), $resolveContext->isForceAbsoluteUri());
+                $this->routerCachingService->storeResolvedUriConstraints($resolveContext, $uriConstraints, $route->getResolvedTags());
                 $this->lastResolvedRoute = $route;
-                $resolvedUriPath = $route->getResolvedUriPath();
-                if ($resolvedUriPath !== null) {
-                    $this->routerCachingService->storeResolvedUriPath($resolvedUriPath, $routeValues);
-                }
-                return $resolvedUriPath;
+                return $resolvedUri;
             }
         }
-        $this->systemLogger->log('Router resolve(): Could not resolve a route for building an URI for the given route values.', LOG_WARNING, $routeValues);
+        $this->systemLogger->log('Router resolve(): Could not resolve a route for building an URI for the given resolve context.', LOG_WARNING, $resolveContext->getRouteValues());
         throw new NoMatchingRouteException('Could not resolve a route and its corresponding URI for the given parameters. This may be due to referring to a not existing package / controller / action while building a link or URI. Refer to log and check the backtrace for more details.', 1301610453);
     }
 

Classes/Mvc/Routing/UriBuilder.php
@@ -11,11 +11,13 @@ namespace Neos\Flow\Mvc\Routing;
  * source code.
  */
 
+use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Http\Request;
 use Neos\Flow\Mvc\ActionRequest;
 use Neos\Flow\Mvc\RequestInterface;
+use Neos\Flow\Mvc\Routing\Dto\ResolveContext;
 use Neos\Utility\Arrays;
-use Neos\Flow\Annotations as Flow;
+use Psr\Http\Message\UriInterface;
 
 /**
  * An URI Builder
@@ -338,7 +340,7 @@ class UriBuilder
      * Builds the URI
      *
      * @param array $arguments optional URI arguments. Will be merged with $this->arguments with precedence to $arguments
-     * @return string The URI
+     * @return UriInterface
      * @api
      */
     public function build(array $arguments = [])
@@ -346,21 +348,18 @@ class UriBuilder
         $arguments = Arrays::arrayMergeRecursiveOverrule($this->arguments, $arguments);
         $arguments = $this->mergeArgumentsWithRequestArguments($arguments);
 
-        $uri = $this->router->resolve($arguments);
-        $this->lastArguments = $arguments;
-        if (!$this->environment->isRewriteEnabled()) {
-            $uri = 'index.php/' . $uri;
-        }
         $httpRequest = $this->request->getHttpRequest();
-        if ($this->createAbsoluteUri === true) {
-            $uri = $httpRequest->getBaseUri() . $uri;
-        } else {
-            $uri = $httpRequest->getScriptRequestPath() . $uri;
-        }
+
+        $uriPathPrefix = $this->environment->isRewriteEnabled() ? '' : 'index.php/';
+        $uriPathPrefix = $httpRequest->getScriptRequestPath() . $uriPathPrefix;
+        $resolveContext = new ResolveContext($httpRequest->getUri(), $arguments, $this->createAbsoluteUri, $uriPathPrefix);
+        $resolvedUri = $this->router->resolve($resolveContext);
         if ($this->section !== '') {
-            $uri .= '#' . $this->section;
+            $resolvedUri = $resolvedUri->withFragment($this->section);
         }
-        return $uri;
+
+        $this->lastArguments = $arguments;
+        return $resolvedUri;
     }
 
     /**

commit 3dbd887b1d2d68f17316afe94f73addde9500fbb

BUGFIX: Respect configured BaseUri when resolving URIs
This fixes a regression intdroduced with #1126 that prevented
the configured `baseUri` from being respected when building
URIs using the `UriBuilder`.

Background:

Instead of using the *Request URI* as base for generated URIs
this fix uses the *Request Base URI*.
Usually the result is the same, but not if a different `baseUri` is
configured from the one being requested.

Fixes: #1184

Classes/Mvc/Routing/Dto/ResolveContext.php
@@ -27,7 +27,7 @@ final class ResolveContext
      *
      * @var UriInterface
      */
-    private $requestUri;
+    private $baseUri;
 
     /**
      * Route values to build the URI, for example ['@action' => 'index', 'someArgument' => 'foo', ...]
@@ -51,14 +51,14 @@ final class ResolveContext
     private $uriPathPrefix;
 
     /**
-     * @param UriInterface $requestUri The currently requested URI, required to fill in parts of the result when resolving absolute URIs
+     * @param UriInterface $baseUri The base URI, retrieved from the current request URI or from configuration, if specified. Required to fill in parts of the result when resolving absolute URIs
      * @param array $routeValues Route values to build the URI, for example ['@action' => 'index', 'someArgument' => 'foo', ...]
      * @param bool $forceAbsoluteUri Whether or not an absolute URI is to be returned
      * @param string $uriPathPrefix A prefix to be prepended to any resolved URI
      */
-    public function __construct(UriInterface $requestUri, array $routeValues, bool $forceAbsoluteUri, string $uriPathPrefix = '')
+    public function __construct(UriInterface $baseUri, array $routeValues, bool $forceAbsoluteUri, string $uriPathPrefix = '')
     {
-        $this->requestUri = $requestUri;
+        $this->baseUri = $baseUri;
         $this->routeValues = $routeValues;
         $this->forceAbsoluteUri = $forceAbsoluteUri;
         $this->uriPathPrefix = $uriPathPrefix;
@@ -66,10 +66,19 @@ final class ResolveContext
 
     /**
      * @return UriInterface
+     * @deprecated This getter has been renamed. @see getBaseUri()
      */
     public function getRequestUri(): UriInterface
     {
-        return $this->requestUri;
+        return $this->getBaseUri();
+    }
+
+    /**
+     * @return UriInterface
+     */
+    public function getBaseUri(): UriInterface
+    {
+        return $this->baseUri;
     }
 
     /**

Classes/Mvc/Routing/Router.php
@@ -171,7 +171,7 @@ class Router implements RouterInterface
         $this->lastResolvedRoute = null;
         $cachedResolvedUriConstraints = $this->routerCachingService->getCachedResolvedUriConstraints($resolveContext);
         if ($cachedResolvedUriConstraints !== false) {
-            return $cachedResolvedUriConstraints->applyTo($resolveContext->getRequestUri(), $resolveContext->isForceAbsoluteUri());
+            return $cachedResolvedUriConstraints->applyTo($resolveContext->getBaseUri(), $resolveContext->isForceAbsoluteUri());
         }
 
         $this->createRoutesFromConfiguration();
@@ -180,7 +180,7 @@ class Router implements RouterInterface
         foreach ($this->routes as $route) {
             if ($route->resolves($resolveContext->getRouteValues()) === true) {
                 $uriConstraints = $route->getResolvedUriConstraints()->withPathPrefix($resolveContext->getUriPathPrefix());
-                $resolvedUri = $uriConstraints->applyTo($resolveContext->getRequestUri(), $resolveContext->isForceAbsoluteUri());
+                $resolvedUri = $uriConstraints->applyTo($resolveContext->getBaseUri(), $resolveContext->isForceAbsoluteUri());
                 $this->routerCachingService->storeResolvedUriConstraints($resolveContext, $uriConstraints, $route->getResolvedTags());
                 $this->lastResolvedRoute = $route;
                 return $resolvedUri;

Classes/Mvc/Routing/UriBuilder.php
@@ -351,7 +351,7 @@ class UriBuilder
 
         $uriPathPrefix = $this->environment->isRewriteEnabled() ? '' : 'index.php/';
         $uriPathPrefix = $httpRequest->getScriptRequestPath() . $uriPathPrefix;
-        $resolveContext = new ResolveContext($httpRequest->getUri(), $arguments, $this->createAbsoluteUri, $uriPathPrefix);
+        $resolveContext = new ResolveContext($httpRequest->getBaseUri(), $arguments, $this->createAbsoluteUri, $uriPathPrefix);
         $resolvedUri = $this->router->resolve($resolveContext);
         if ($this->section !== '') {
             $resolvedUri = $resolvedUri->withFragment($this->section);

Tests/Functional/Mvc/RoutingTest.php
@@ -286,8 +286,8 @@ class RoutingTest extends FunctionalTestCase
      */
     public function resolveTests(array $routeValues, $expectedResolvedRouteName, $expectedResolvedUriPath = null)
     {
-        $requestUri = new Uri('http://localhost');
-        $resolvedUriPath = $this->router->resolve(new ResolveContext($requestUri, $routeValues, false));
+        $baseUri = new Uri('http://localhost');
+        $resolvedUriPath = $this->router->resolve(new ResolveContext($baseUri, $routeValues, false));
         $resolvedRoute = $this->router->getLastResolvedRoute();
         if ($expectedResolvedRouteName === null) {
             if ($resolvedRoute !== null) {
@@ -352,8 +352,8 @@ class RoutingTest extends FunctionalTestCase
             '@action' => 'index',
             '@format' => 'html'
         ];
-        $requestUri = new Uri('http://localhost');
-        $actualResult = $this->router->resolve(new ResolveContext($requestUri, $routeValues, false));
+        $baseUri = new Uri('http://localhost');
+        $actualResult = $this->router->resolve(new ResolveContext($baseUri, $routeValues, false));
 
         $this->assertSame('neos/flow/test/http/foo', (string)$actualResult);
     }
@@ -383,8 +383,8 @@ class RoutingTest extends FunctionalTestCase
             ]
         ];
         $this->router->setRoutesConfiguration($routesConfiguration);
-        $requestUri = new Uri('http://localhost');
-        $actualResult = $this->router->resolve(new ResolveContext($requestUri, $routeValues, false));
+        $baseUri = new Uri('http://localhost');
+        $actualResult = $this->router->resolve(new ResolveContext($baseUri, $routeValues, false));
         $this->assertSame('custom/uri/pattern', (string)$actualResult);
 
         // reset router configuration for following tests

Tests/Unit/Mvc/Routing/RouterTest.php
@@ -60,7 +60,7 @@ class RouterTest extends UnitTestCase
     /**
      * @var UriInterface|\PHPUnit_Framework_MockObject_MockObject
      */
-    protected $mockRequestUri;
+    protected $mockBaseUri;
 
     /**
      * @return void
@@ -79,8 +79,8 @@ class RouterTest extends UnitTestCase
 
         $this->mockHttpRequest = $this->getMockBuilder(Request::class)->disableOriginalConstructor()->getMock();
 
-        $this->mockRequestUri = $this->getMockBuilder(UriInterface::class)->getMock();
-        $this->mockHttpRequest->expects($this->any())->method('getUri')->will($this->returnValue($this->mockRequestUri));
+        $this->mockBaseUri = $this->getMockBuilder(UriInterface::class)->getMock();
+        $this->mockHttpRequest->expects($this->any())->method('getBaseUri')->will($this->returnValue($this->mockBaseUri));
 
         $this->mockActionRequest = $this->getMockBuilder(ActionRequest::class)->disableOriginalConstructor()->getMock();
     }
@@ -105,7 +105,7 @@ class RouterTest extends UnitTestCase
 
         // this we actually want to know
         $router->expects($this->once())->method('createRoutesFromConfiguration');
-        $router->resolve(new ResolveContext($this->mockRequestUri, [], false));
+        $router->resolve(new ResolveContext($this->mockBaseUri, [], false));
     }
 
     /**
@@ -194,7 +194,7 @@ class RouterTest extends UnitTestCase
         $router->expects($this->once())->method('createRoutesFromConfiguration');
         $router->_set('routes', $mockRoutes);
 
-        $resolvedUri = $router->resolve(new ResolveContext($this->mockRequestUri, $routeValues, false));
+        $resolvedUri = $router->resolve(new ResolveContext($this->mockBaseUri, $routeValues, false));
         $this->assertSame('route2', $resolvedUri->getPath());
     }
 
@@ -219,7 +219,7 @@ class RouterTest extends UnitTestCase
 
         $router->_set('routes', $mockRoutes);
 
-        $router->resolve(new ResolveContext($this->mockRequestUri, [], false));
+        $router->resolve(new ResolveContext($this->mockBaseUri, [], false));
     }
 
     /**
@@ -242,7 +242,7 @@ class RouterTest extends UnitTestCase
 
 
         $routeValues = ['some' => 'route values'];
-        $resolveContext = new ResolveContext($this->mockRequestUri, $routeValues, false);
+        $resolveContext = new ResolveContext($this->mockBaseUri, $routeValues, false);
         $mockRoute1 = $this->getMockBuilder(Route::class)->getMock();
         $mockRoute1->expects($this->once())->method('resolves')->with($routeValues)->will($this->returnValue(false));
         $mockRoute2 = $this->getMockBuilder(Route::class)->getMock();
@@ -269,7 +269,7 @@ class RouterTest extends UnitTestCase
         $routeValues = ['some' => 'route values'];
         $mockCachedResolvedUriConstraints = UriConstraints::create()->withPath('cached/path');
 
-        $resolveContext = new ResolveContext($this->mockRequestUri, $routeValues, false);
+        $resolveContext = new ResolveContext($this->mockBaseUri, $routeValues, false);
 
         $mockRouterCachingService = $this->getMockBuilder(RouterCachingService::class)->getMock();
         $mockRouterCachingService->expects($this->any())->method('getCachedResolvedUriConstraints')->with($resolveContext)->will($this->returnValue($mockCachedResolvedUriConstraints));
@@ -292,7 +292,7 @@ class RouterTest extends UnitTestCase
         $routeValues = ['some' => 'route values'];
         $mockResolvedUriConstraints = UriConstraints::create()->withPath('resolved/path');
 
-        $resolveContext = new ResolveContext($this->mockRequestUri, $routeValues, false);
+        $resolveContext = new ResolveContext($this->mockBaseUri, $routeValues, false);
 
         $mockRoute1 = $this->getMockBuilder(Route::class)->getMock();
         $mockRoute1->expects($this->once())->method('resolves')->with($routeValues)->will($this->returnValue(false));

Tests/Unit/Mvc/Routing/UriBuilderTest.php
@@ -12,10 +12,11 @@ namespace Neos\Flow\Tests\Unit\Mvc\Routing;
  */
 
 use Neos\Flow\Http;
+use Neos\Flow\Mvc\ActionRequest;
 use Neos\Flow\Mvc\Routing\Dto\ResolveContext;
 use Neos\Flow\Tests\UnitTestCase;
 use Neos\Flow\Mvc;
-use Neos\Utility;
+use Neos\Flow\Utility;
 use Psr\Http\Message\UriInterface;
 
 /**
@@ -41,7 +42,7 @@ class UriBuilderTest extends UnitTestCase
     /**
      * @var UriInterface|\PHPUnit_Framework_MockObject_MockObject
      */
-    protected $mockRequestUri;
+    protected $mockBaseUri;
 
     /**
      * @var Mvc\ActionRequest|\PHPUnit_Framework_MockObject_MockObject
@@ -66,8 +67,8 @@ class UriBuilderTest extends UnitTestCase
     {
         $this->mockHttpRequest = $this->getMockBuilder(Http\Request::class)->disableOriginalConstructor()->getMock();
 
-        $this->mockRequestUri = $this->getMockBuilder(UriInterface::class)->getMock();
-        $this->mockHttpRequest->expects($this->any())->method('getUri')->will($this->returnValue($this->mockRequestUri));
+        $this->mockBaseUri = $this->getMockBuilder(UriInterface::class)->getMock();
+        $this->mockHttpRequest->expects($this->any())->method('getBaseUri')->will($this->returnValue($this->mockBaseUri));
 
         $this->mockRouter = $this->createMock(Mvc\Routing\RouterInterface::class);
 
@@ -711,6 +712,21 @@ class UriBuilderTest extends UnitTestCase
         $this->uriBuilder->build();
 
         $this->assertEquals($expectedArguments, $this->uriBuilder->getLastArguments());
+    }
+
+    /**
+     * @test
+     */
+    public function buildPassesBaseUriToRouter()
+    {
+        $this->mockHttpRequest->expects($this->atLeastOnce())->method('getBaseUri')->will($this->returnValue($this->mockBaseUri));
+
+        $this->mockRouter->expects($this->once())->method('resolve')->willReturnCallback(function (ResolveContext $resolveContext) {
+            $this->assertSame($this->mockBaseUri, $resolveContext->getBaseUri());
+            return $this->getMockBuilder(UriInterface::class)->getMock();
+        });
+
+        $this->uriBuilder->build();
     }
 
     /**