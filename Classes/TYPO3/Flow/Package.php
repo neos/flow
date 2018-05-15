neos/flow-development-collection

BUGFIX: Clear internal ResourceRepository state after persisting changes
The ResourceRepository kept an internal record of added and removed
resource objects and used them in certain cases when fetching objects.

This change makes sure that these records are reset after objects have
been persisted. Otherwise false duplicates are returned within one
request.

Neos.Flow/Classes/Package.php
@@ -13,6 +13,7 @@ namespace Neos\Flow;
 
 use Neos\Flow\Package\Package as BasePackage;
 use Neos\Flow\ResourceManagement\ResourceManager;
+use Neos\Flow\ResourceManagement\ResourceRepository;
 
 /**
  * The Flow Package
@@ -118,5 +119,8 @@ class Package extends BasePackage
                 $bootstrap->getObjectManager()->get(\Neos\Flow\Cache\CacheManager::class)->flushCaches();
             });
         });
+
+        $dispatcher->connect(Persistence\Doctrine\PersistenceManager::class, 'allObjectsPersisted', ResourceRepository::class, 'resetAfterPersistingChanges');
+        $dispatcher->connect(Persistence\Generic\PersistenceManager::class, 'allObjectsPersisted', ResourceRepository::class, 'resetAfterPersistingChanges');
     }
 }

neos/flow

Classes/TYPO3/Flow/Package.php
@@ -15,6 +15,7 @@ use TYPO3\Flow\Package\Package as BasePackage;
 use TYPO3\Flow\Package\PackageInterface;
 use TYPO3\Flow\Package\PackageManagerInterface;
 use TYPO3\Flow\Resource\ResourceManager;
+use TYPO3\Flow\Resource\ResourceRepository;
 use TYPO3\Fluid\Core\Parser\TemplateParser;
 
 /**
@@ -130,5 +131,8 @@ class Package extends BasePackage
                 $bootstrap->getObjectManager()->get(\TYPO3\Flow\Cache\CacheManager::class)->flushCaches();
             });
         });
+
+        $dispatcher->connect(Persistence\Doctrine\PersistenceManager::class, 'allObjectsPersisted', ResourceRepository::class, 'resetAfterPersistingChanges');
+        $dispatcher->connect(Persistence\Generic\PersistenceManager::class, 'allObjectsPersisted', ResourceRepository::class, 'resetAfterPersistingChanges');
     }
 }