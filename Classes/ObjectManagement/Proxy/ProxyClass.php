neos/flow

commit b83b70c6c6745ef443efbc5a3fa114c72622041f

TASK: Render proxy methods with laminas-code
Flow now uses laminas/laminas-code for rendering proxy methods. The Dependency Injection Proxy Class Builder was refactored and the classes ProxyConstructor and ProxyMethod were replaced by new implementations called ProxyConstructorGenerator and ProxyMethodGenerator respectively.

Resolves: #3064

Classes/ObjectManagement/Proxy/ProxyClass.php
@@ -11,7 +11,9 @@ namespace Neos\Flow\ObjectManagement\Proxy;
  * source code.
  */
 
+use Laminas\Code\Reflection\MethodReflection;
 use Neos\Flow\Annotations as Flow;
+use Neos\Flow\ObjectManagement\Exception\CannotBuildObjectException;
 use Neos\Flow\Reflection\ClassReflection;
 use Neos\Flow\Reflection\ReflectionService;
 
@@ -44,7 +46,7 @@ class ProxyClass
     protected $fullOriginalClassName;
 
     /**
-     * @var ProxyConstructor
+     * @var ProxyConstructorGenerator
      */
     protected $constructor;
 
@@ -109,15 +111,21 @@ class ProxyClass
     }
 
     /**
-     * Returns the ProxyConstructor for this ProxyClass. Creates it if needed.
+     * Returns the ProxyConstructorGenerator for this ProxyClass. Creates it if needed.
      *
-     * @return ProxyConstructor
+     * @return ProxyConstructorGenerator
+     * @throws \ReflectionException
+     * @throws CannotBuildObjectException
      */
-    public function getConstructor(): ProxyConstructor
+    public function getConstructor(): ProxyConstructorGenerator
     {
         if (!isset($this->constructor)) {
-            $this->constructor = new ProxyConstructor($this->fullOriginalClassName);
-            $this->constructor->injectReflectionService($this->reflectionService);
+            if (method_exists($this->fullOriginalClassName, '__construct')) {
+                $this->constructor = ProxyConstructorGenerator::fromReflection(new MethodReflection($this->fullOriginalClassName, '__construct'));
+            } else {
+                $this->constructor = new ProxyConstructorGenerator();
+                $this->constructor->setFullOriginalClassName($this->fullOriginalClassName);
+            }
         }
         return $this->constructor;
     }
@@ -125,18 +133,21 @@ class ProxyClass
     /**
      * Returns the named ProxyMethod for this ProxyClass. Creates it if needed.
      *
-     * @param string $methodName The name of the methods to return
-     * @return ProxyMethod
+     * @throws \ReflectionException
      */
-    public function getMethod(string $methodName): ProxyConstructor|ProxyMethod
+    public function getMethod(string $methodName): ProxyMethodGenerator|ProxyConstructorGenerator
     {
         if ($methodName === '__construct') {
             return $this->getConstructor();
         }
         if (!isset($this->methods[$methodName])) {
-            $this->methods[$methodName] = new ProxyMethod($this->fullOriginalClassName, $methodName);
-            $this->methods[$methodName]->injectReflectionService($this->reflectionService);
+            if (method_exists($this->fullOriginalClassName, $methodName)) {
+                $this->methods[$methodName] = ProxyMethodGenerator::fromReflection(new MethodReflection($this->fullOriginalClassName, $methodName));
+            } else {
+                $this->methods[$methodName] = new ProxyMethodGenerator($methodName);
+            }
         }
+        $this->methods[$methodName]->getDocBlock()?->setWordWrap(false);
         return $this->methods[$methodName];
     }
 
@@ -203,6 +214,7 @@ class ProxyClass
      * Renders and returns the PHP code for this ProxyClass.
      *
      * @return string
+     * @throws CannotBuildObjectException
      */
     public function render(): string
     {
@@ -221,22 +233,40 @@ class ProxyClass
         $constantsCode = $this->renderConstantsCode();
         $propertiesCode = $this->renderPropertiesCode();
         $traitsCode = $this->renderTraitsCode();
+        $methodsCode = '';
 
-        $methodsCode = isset($this->constructor) ? $this->constructor->render() : '';
-        foreach ($this->methods as $proxyMethod) {
-            $methodsCode .= $proxyMethod->render();
+        $constructorBodyCode = isset($this->constructor) ? $this->constructor->renderBodyCode() : '';
+        if ($constructorBodyCode !== '') {
+            $this->constructor->setBody($constructorBodyCode);
+            $methodsCode .= PHP_EOL . $this->constructor->generate();
+
+            foreach (class_implements($this->fullOriginalClassName) as $interface) {
+                if (method_exists($interface, '__construct')) {
+                    throw new CannotBuildObjectException(sprintf('The class "%s" implements the interface "%s" which has a constructor. Proxy classes implementing an interface containing a constructor is not supported and constructors interfaces are generally strongly discouraged.', $this->fullOriginalClassName, $interface), 1685433328);
+                }
+            }
         }
 
-        if ($methodsCode . $constantsCode === '') {
+        foreach ($this->methods as $proxyMethod) {
+            assert($proxyMethod instanceof ProxyMethodGenerator);
+            $methodBodyCode = $proxyMethod->renderBodyCode();
+            if ($methodBodyCode !== '') {
+                $proxyMethod->setBody($methodBodyCode);
+                $methodsCode .= PHP_EOL . $proxyMethod->generate();
+            }
+        }
+
+        if (trim($methodsCode) . $constantsCode === '') {
             return '';
         }
+
         return $this->buildClassDocumentation() .
-            $classModifier . 'class ' . $proxyClassName . ' extends ' . $originalClassName . ' implements ' . implode(', ', array_unique($this->interfaces)) . " {\n\n" .
+            $classModifier . 'class ' . $proxyClassName . ' extends ' . $originalClassName . ' implements ' . implode(', ', array_unique($this->interfaces)) . " {" . PHP_EOL . PHP_EOL .
             $traitsCode .
             $constantsCode .
             $propertiesCode .
-            $methodsCode .
-            '}';
+            rtrim($methodsCode) .
+            PHP_EOL. '}';
     }
 
     /**
@@ -267,7 +297,7 @@ class ProxyClass
     {
         $code = '';
         foreach ($this->constants as $name => $valueCode) {
-            $code .= '    const ' . $name . ' = ' . $valueCode . ";\n\n";
+            $code .= '    const ' . $name . ' = ' . $valueCode . ";\n";
         }
         return $code;
     }
@@ -284,7 +314,7 @@ class ProxyClass
             if (!empty($attributes['docComment'])) {
                 $code .= '    ' . $attributes['docComment'] . "\n";
             }
-            $code .= '    ' . $attributes['visibility'] . ' $' . $name . ' = ' . $attributes['initialValueCode'] . ";\n\n";
+            $code .= '    ' . $attributes['visibility'] . ' $' . $name . ' = ' . $attributes['initialValueCode'] . ";\n";
         }
         return $code;
     }

Classes/ObjectManagement/Proxy/ProxyMethodGenerator.php
@@ -1,0 +1,164 @@
+<?php
+namespace Neos\Flow\ObjectManagement\Proxy;
+
+/*
+ * This file is part of the Neos.Flow package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Laminas\Code\Generator\MethodGenerator;
+
+/**
+ * Generator for proxy methods
+ */
+class ProxyMethodGenerator extends MethodGenerator
+{
+    protected string $addedPreParentCallCode = '';
+    protected string $addedPostParentCallCode = '';
+
+    protected string $fullOriginalClassName = '';
+
+    public static function fromReflection(\Laminas\Code\Reflection\MethodReflection $reflectionMethod): static
+    {
+        $instance = parent::fromReflection($reflectionMethod);
+        assert($instance instanceof static);
+        $instance->fullOriginalClassName = $reflectionMethod->getDeclaringClass()->getName();
+        return $instance;
+    }
+
+    public static function copyMethodSignature(\Laminas\Code\Reflection\MethodReflection $reflectionMethod): static
+    {
+        $instance = parent::copyMethodSignature($reflectionMethod);
+        assert($instance instanceof static);
+        $instance->fullOriginalClassName = $reflectionMethod->getDeclaringClass()->getName();
+        return $instance;
+    }
+
+    public function getFullOriginalClassName(): string
+    {
+        return $this->fullOriginalClassName;
+    }
+
+    public function setFullOriginalClassName(string $fullOriginalClassName): void
+    {
+        $this->fullOriginalClassName = $fullOriginalClassName;
+    }
+
+    /**
+     * Adds PHP code to the body of this proxy method which will be executed before a possible parent call.
+     */
+    public function addPreParentCallCode(string $code): void
+    {
+        $this->addedPreParentCallCode .= rtrim($code) . PHP_EOL;
+    }
+
+    /**
+     * Adds PHP code to the body of this proxy method which will be executed after a possible parent call.
+     */
+    public function addPostParentCallCode(string $code): void
+    {
+        $this->addedPostParentCallCode .= rtrim($code) . PHP_EOL;
+    }
+
+    public function generate(): string
+    {
+        if ($this->body === '') {
+            $this->body = $this->renderBodyCode();
+        }
+        return parent::generate();
+    }
+
+    public function renderBodyCode(): string
+    {
+        if ((trim($this->addedPreParentCallCode) === '' && trim($this->addedPostParentCallCode) === '')) {
+            return '';
+        }
+
+        $callParentMethodCode = $this->buildCallParentMethodCode($this->fullOriginalClassName, $this->name);
+        $returnTypeIsVoid = ((string)$this->getReturnType() === 'void');
+        $code = $this->addedPreParentCallCode;
+        if ($this->addedPostParentCallCode !== '') {
+            if ($returnTypeIsVoid) {
+                if ($callParentMethodCode !== '') {
+                    $code .= '    ' . $callParentMethodCode;
+                }
+            } else {
+                $code .= '$result = ' . ($callParentMethodCode === '' ? "NULL;\n" : $callParentMethodCode);
+            }
+            $code .= $this->addedPostParentCallCode;
+            if (!$returnTypeIsVoid) {
+                $code .= "return \$result;\n";
+            }
+        } elseif (!$returnTypeIsVoid && $callParentMethodCode !== '') {
+            $code .= 'return ' . $callParentMethodCode . ";\n";
+        }
+        return $code;
+    }
+
+    /**
+     * Tells if enough code was provided (yet) so that this method would actually be rendered
+     * if render() is called.
+     *
+     * @return bool true if there is any code to render, otherwise false
+     */
+    public function willBeRendered(): bool
+    {
+        return ($this->addedPreParentCallCode !== '' || $this->addedPostParentCallCode !== '');
+    }
+
+    /**
+     * Builds the PHP code for the parameters of the specified method to be
+     * used in a method interceptor in the proxy class
+     *
+     * @param string|null $fullClassName Name of the class the method is declared in
+     * @param string|null $methodName Name of the method to create the parameters code for
+     * @param bool $addTypeAndDefaultValue If the type and default value for each parameter should be rendered
+     * @return string A comma separated list of parameters
+     */
+    public function buildMethodParametersCode(?string $fullClassName, ?string $methodName, bool $addTypeAndDefaultValue): string
+    {
+        if ($fullClassName === null || $methodName === null) {
+            return '';
+        }
+
+        if (!method_exists($fullClassName, $methodName)) {
+            return '';
+        }
+
+        $methodParametersCode = '';
+        $parameterOutput = [];
+        $parameters = $this->getParameters();
+        if (!empty($parameters)) {
+            foreach ($parameters as $parameter) {
+                if ($addTypeAndDefaultValue) {
+                    $parameterOutput[] = $parameter->generate();
+                } else {
+                    $parameterOutput[] = '$' . $parameter->getName();
+                }
+            }
+            $methodParametersCode .= implode(', ', $parameterOutput);
+        }
+
+        return $methodParametersCode;
+    }
+
+    /**
+     * Builds PHP code which calls the original (i.e. parent) method after the added code has been executed.
+     *
+     * @param string $fullClassName Fully qualified name of the original class
+     * @param string $methodName Name of the original method
+     * @return string PHP code
+     */
+    protected function buildCallParentMethodCode(string $fullClassName, string $methodName): string
+    {
+        if (!method_exists($fullClassName, $methodName)) {
+            return '';
+        }
+        return 'parent::' . $methodName . '(' . $this->buildMethodParametersCode($fullClassName, $methodName, false) . ");\n";
+    }
+}

Classes/Aop/Builder/AdvisedConstructorInterceptorBuilder.php
@@ -39,28 +39,25 @@ class AdvisedConstructorInterceptorBuilder extends AbstractMethodInterceptorBuil
 
         $declaringClassName = $methodMetaInformation[$methodName]['declaringClassName'];
         $proxyMethod = $this->compiler->getProxyClass($targetClassName)->getConstructor();
-        if ($declaringClassName !== $targetClassName) {
-            $proxyMethod->setMethodParametersCode($proxyMethod->buildMethodParametersCode($declaringClassName, $methodName, true));
-        }
 
         $groupedAdvices = $methodMetaInformation[$methodName]['groupedAdvices'];
         $advicesCode = $this->buildAdvicesCode($groupedAdvices, $methodName, $targetClassName, $declaringClassName);
 
-        $proxyMethod->addPreParentCallCode('
-        if (isset($this->Flow_Aop_Proxy_methodIsInAdviceMode[\'' . $methodName . '\'])) {
-');
-        $proxyMethod->addPostParentCallCode('
+        $proxyMethod->addPreParentCallCode(<<<PHP
+        if (isset(\$this->Flow_Aop_Proxy_methodIsInAdviceMode['{$methodName}'])) {
+        PHP);
+        $proxyMethod->addPostParentCallCode(<<<PHP
         } else {
-            $this->Flow_Aop_Proxy_methodIsInAdviceMode[\'' . $methodName . '\'] = true;
+            \$this->Flow_Aop_Proxy_methodIsInAdviceMode['{$methodName}'] = true;
             try {
-            ' . $advicesCode . '
-            } catch (\Exception $exception) {
-                unset($this->Flow_Aop_Proxy_methodIsInAdviceMode[\'' . $methodName . '\']);
-                throw $exception;
+            {$advicesCode}
+            } catch (\Exception \$exception) {
+                unset(\$this->Flow_Aop_Proxy_methodIsInAdviceMode['{$methodName}']);
+                throw \$exception;
             }
-            unset($this->Flow_Aop_Proxy_methodIsInAdviceMode[\'' . $methodName . '\']);
+            unset(\$this->Flow_Aop_Proxy_methodIsInAdviceMode['{$methodName}']);
             return;
         }
-');
+        PHP);
     }
 }

Classes/Aop/Builder/AdvisedMethodInterceptorBuilder.php
@@ -11,8 +11,10 @@ namespace Neos\Flow\Aop\Builder;
  * source code.
  */
 
+use Laminas\Code\Generator\MethodGenerator;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Aop\Exception;
+use Neos\Flow\ObjectManagement\Proxy\ProxyMethodGenerator;
 
 /**
  * An AOP interceptor code builder for methods enriched by advices.
@@ -38,30 +40,31 @@ class AdvisedMethodInterceptorBuilder extends AbstractMethodInterceptorBuilder
 
         $declaringClassName = $methodMetaInformation[$methodName]['declaringClassName'];
         $proxyMethod = $this->compiler->getProxyClass($targetClassName)->getMethod($methodName);
-        if ($proxyMethod->isPrivate()) {
+        if ($proxyMethod->getVisibility() === ProxyMethodGenerator::VISIBILITY_PRIVATE) {
             throw new Exception(sprintf('The %s cannot build interceptor code for private method %s::%s(). Please change the scope to at least protected or adjust the pointcut expression in the corresponding aspect.', __CLASS__, $targetClassName, $methodName), 1593070574);
         }
         if ($declaringClassName !== $targetClassName) {
-            $proxyMethod->setMethodParametersCode($proxyMethod->buildMethodParametersCode($declaringClassName, $methodName, true));
+            $originalMethod = MethodGenerator::copyMethodSignature(new \Laminas\Code\Reflection\MethodReflection($declaringClassName, $methodName));
+            $proxyMethod->setParameters($originalMethod->getParameters());
         }
 
         $groupedAdvices = $methodMetaInformation[$methodName]['groupedAdvices'];
         $advicesCode = $this->buildAdvicesCode($groupedAdvices, $methodName, $targetClassName, $declaringClassName);
 
-        $proxyMethod->addPreParentCallCode('
-    if (isset($this->Flow_Aop_Proxy_methodIsInAdviceMode[\'' . $methodName . '\'])) {
-');
-        $proxyMethod->addPostParentCallCode('
-    } else {
-        $this->Flow_Aop_Proxy_methodIsInAdviceMode[\'' . $methodName . '\'] = true;
-        try {
-        ' . $advicesCode . '
-        } catch (\Exception $exception) {
-            unset($this->Flow_Aop_Proxy_methodIsInAdviceMode[\'' . $methodName . '\']);
-            throw $exception;
+        $proxyMethod->addPreParentCallCode(<<<PHP
+        if (isset(\$this->Flow_Aop_Proxy_methodIsInAdviceMode['{$methodName}'])) {
+        PHP);
+        $proxyMethod->addPostParentCallCode(<<<PHP
+        } else {
+            \$this->Flow_Aop_Proxy_methodIsInAdviceMode['{$methodName}'] = true;
+            try {
+            {$advicesCode}
+            } catch (\Exception \$exception) {
+                unset(\$this->Flow_Aop_Proxy_methodIsInAdviceMode['{$methodName}']);
+                throw \$exception;
+            }
+            unset(\$this->Flow_Aop_Proxy_methodIsInAdviceMode['{$methodName}']);
         }
-        unset($this->Flow_Aop_Proxy_methodIsInAdviceMode[\'' . $methodName . '\']);
-    }
-');
+        PHP);
     }
 }

commit 8cca18fbd379c12f2d05dbfe0873ff733bc26088

BUGFIX: Replacement proxy methods rendered again
This fixes a bug introduced in b83b70c6 switching to laminuas-code.
A proxy method can replace the full body of an existing method or even
be a fully new method, in which case only `body` will be set in the
proxy method. We still want those to be generated. This for example
currently breaks the CompileStatic feature, as those methods do not
get rendered anymore resulting in worse performance in Production
context compared to before.

This fix renders a proxy method also when a body was set for it, but
still skips it if neither pre/post nor body is set.

Fixes: #3099

Classes/ObjectManagement/Proxy/ProxyClass.php
@@ -249,9 +249,7 @@ class ProxyClass
 
         foreach ($this->methods as $proxyMethod) {
             assert($proxyMethod instanceof ProxyMethodGenerator);
-            $methodBodyCode = $proxyMethod->renderBodyCode();
-            if ($methodBodyCode !== '') {
-                $proxyMethod->setBody($methodBodyCode);
+            if ($proxyMethod->willBeRendered()) {
                 $methodsCode .= PHP_EOL . $proxyMethod->generate();
             }
         }

Classes/ObjectManagement/Proxy/ProxyMethodGenerator.php
@@ -108,7 +108,7 @@ class ProxyMethodGenerator extends MethodGenerator
      */
     public function willBeRendered(): bool
     {
-        return ($this->addedPreParentCallCode !== '' || $this->addedPostParentCallCode !== '');
+        return ($this->addedPreParentCallCode !== '' || $this->addedPostParentCallCode !== '' || $this->body !== '');
     }
 
     /**